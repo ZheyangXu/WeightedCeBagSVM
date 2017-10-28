88d99b1c5e451ec9f0b6f526dbc5f28f81ac737d
Ecrire <jishengxzy@hotmail.com>
2017-10-28 15:39:30 +0800

    modify CeBag2best
    Remove unused library.

--- CeBag2best.py
+++ CeBag2best.py
@@ -1,11 +1,9 @@
 # coding=utf-8
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import axes3d
 
 import time
-import os
+ os
 
 from sklearn import datasets
 from sklearn.svm import SVC
